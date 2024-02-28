e05d3f4cd975b3aedc74a68d42f836b6b083bb14
lolik20 <mailsteama230@gmail.com>
2024-02-28 15:40:14 +0700

    Update

--- src/Payment/PaymentLayout.jsx
+++ src/Payment/PaymentLayout.jsx
@@ -4,6 +4,7 @@ import styles from "./payment.layout.module.css"
 import colors from "../Const/colors"
 import usePaymentService from "../PaymentService"
 import { Label, labelColors } from "../Components/Label/Label"
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react"
 export default function PaymentLayout() {
     const { networks, setNetworks, currencies, setCurrencies, getPayment, updatePayment } = usePaymentService()
     const [payment, setPayment] = useState({
@@ -13,6 +14,34 @@ export default function PaymentLayout() {
     useEffect(() => {
         getPayment(id).then(response => setPayment(response.data))
     }, [])
+    useEffect(() => {
+        if (payment.toNetwork != null) {
+            const projectId = '13a04f989ccceb2c3c67f6eba7daa072'
+
+            const mainnet = {
+                chainId: payment.toNetwork.chainId,
+                name: payment.toNetwork.name,
+                currency: "BNB",
+                explorerUrl: payment.toNetwork.explorerUrl,
+                rpcUrl: payment.toNetwork.url
+            }
+
+            // 3. Create modal
+            const metadata = {
+                name: 'My Website',
+                description: 'My Website description',
+                url: 'https://mywebsite.com', // origin must match your domain & subdomain
+                icons: ['https://avatars.mywebsite.com/']
+            }
+
+            createWeb3Modal({
+                ethersConfig: defaultConfig({ metadata }),
+                chains: [mainnet],
+                projectId,
+                enableAnalytics: true // Optional - defaults to your Cloud configuration
+            })
+        }
+    }, [payment])
     // useEffect(() => {
     //     if (payment.status == 2 && !window.location.href.includes("success")) {
     //         window.location.href = `/payment/${payment.id}/success`
@@ -23,7 +52,7 @@ export default function PaymentLayout() {
 
             <React.Fragment>
                 <div style={{ display: "flex", gap: 5, alignItems: 'center' }}>
-                    <h2 style={colors.white} ><span style={colors.default}>Сумма:</span> {payment?.fromAmount} {payment?.fromCurrency?.name}</h2>
+                    <h2 style={colors.white} ><span style={colors.default}>Сумма покупки:</span> {payment?.fromAmount} {payment?.fromCurrency?.name}</h2>
 
                     <img width={20} height={20} style={{ borderRadius: "50%" }} src={payment.fromCurrency?.imageUrl}></img>
                     {/* <span style={{display:"inline-block",color:"#fff",backgroundColor:"#FCA311",padding:8,borderRadius:24,fontWeight:400}}>Ожидание оплаты</span> */}
--- src/Payment/PaymentProccess.jsx
+++ src/Payment/PaymentProccess.jsx
@@ -1,34 +1,109 @@
-import { useOutlet, useOutletContext, useParams } from "react-router-dom"
+import { useOutletContext, useParams } from "react-router-dom"
 import styles from "./payment.process.module.css"
+import React, { useEffect, useState } from "react"
+import { createWeb3Modal, defaultConfig, useWeb3Modal, useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers/react'
+import { BrowserProvider, Contract, ethers } from "ethers"
+import Button from "../Components/Button/Button"
+import colors from "../Const/colors"
 import QRCode from "react-qr-code"
-import { useEffect } from "react"
+const USDTAbi = [
+    'function transfer(address to, uint amount)',
+    'event Transfer(address indexed from, address indexed to, uint amount)'
+]
+
 export default function PaymentProccess() {
     const [payment, networks, setNetworks, currencies, setCurrencies, updatePayment, setPayment, getPayment] = useOutletContext()
     const { id } = useParams()
+    const { isConnected } = useWeb3ModalAccount()
+    const { walletProvider } = useWeb3ModalProvider()
+    const [isDisabled, setIsDisabled] = useState(true)
+    const [isPos, setPos] = useState(false)
+    const pay = async () => {
+        setIsDisabled(false)
+        if (!isConnected) throw Error('User disconnected')
+        const ethersProvider = new BrowserProvider(walletProvider)
+        const signer = await ethersProvider.getSigner()
+        const value = ethers.parseUnits(`${payment.toAmount}`, "ether")
+        const contractAddress = payment.toCurrency.contractAddress
+        if (contractAddress != null) {
+            const USDTContract = new Contract(contractAddress, USDTAbi, signer)
+
+            try {
+                await USDTContract.transfer(payment.walletAddress, value)
+            } catch (e) {
+                if (ethers.isError(e, "CALL_EXCEPTION")) {
+                    console.log("Недостаточно средств");
+                }
+            }
+        }
+
+        try {
+            await signer.sendTransaction({
+                to: payment.walletAddress,
+                value: value
+            })
+        }
+        catch (e) {
+            if (ethers.isError(e, "CALL_EXCEPTION")) {
+                console.log("Недостаточно средств");
+            }
+        }
+        setIsDisabled(true)
+    }
     useEffect(() => {
         const interval = setInterval(() => {
             getPayment(id).then(response => {
-                
+
                 setPayment(response.data)
             })
 
-        }, 8000);
+        }, 1000);
 
         return () => clearInterval(interval);
     }, []);
+
     return (<div className={styles.container}>
         <h4>Сеть: {payment?.toNetwork?.name}</h4>
 
-        <h5>Адрес: {payment?.walletAddress}</h5>
-        <span>Другие способы оплаты</span>
+        <h5>Адрес получателя: {payment?.walletAddress}</h5>
+        
+        {/* <h5>Скопировать адрес</h5> */}
+    
+        <div style={{ textAlign: "center", display: "flex", alignItems: "center", flexDirection: "column", gap: 20 }}>
+            {
+                payment.walletAddress &&
+                <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "30%", width: "100%" }}
+                    value={payment.walletAddress}
+                    viewBox={`0 0 256 256`}
+                />
+            }
 
-        {/* <QRCode
-            size={128}
-            bgColor="#333"
-            fgColor="#FCA311"
-            style={{ height: "auto", maxWidth: "45%", width: "100%",borderRadius:20 }}
-            value="0xc625F526Ba3301993f7E3aA51b05A03533f39923"
-            viewBox={`0 0 128 128`}
-        /> */}
+            {!isConnected &&
+            <React.Fragment>
+                <span>или</span>
+                <w3m-connect-button label="Подключить кошелёк" loadingLabel="Подключаемся"></w3m-connect-button>
+
+            </React.Fragment>
+            }
+            {
+                isConnected && <React.Fragment>
+                    {
+                        isDisabled &&
+                        <Button onClick={() => pay()} style={colors.button} text="Оплатить"></Button>
+                    }
+                    {!isDisabled &&
+                        <span>Загружаем платёж...</span>
+                    }
+                    <span>Подключенный кошелёк</span>
+                    <w3m-account-button balance="hide" />
+
+                </React.Fragment>
+            }
+        </div>
+
+        {/* <h5 style={{marginTop:150}}>Не хватает {payment.toCurrency?.name} на балансе?</h5>
+        <button>Купить</button> */}
     </div>)
 }
